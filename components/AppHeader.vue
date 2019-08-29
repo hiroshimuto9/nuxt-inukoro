8531559a443bdb15afea0dc074f491f87f741786
mutohiroshi <mutohiroshi@team-lab.com>
2019-08-29 12:35:03 +0900

    [add] ヘッダーの追加(htmlのみ)

---
+++ components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+	<nav class="navbar">
+		<div class="container">
+			<div class="navbar-brand">
+				<nuxt-link to="/breeds" class="navbar-item">イヌコロ</nuxt-link>
+			</div>
+		</div>
+	</nav>
+</template>
