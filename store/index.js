f1e099f88d34941b869cf546cd61bef775bcb33a
mutohiroshi <mutohiroshi@team-lab.com>
2019-08-30 12:33:17 +0900

    [add] ページングの追加

--- store/index.js
+++ store/index.js
@@ -5,14 +5,18 @@ const appStore = () => {
 		state: {
 			breed_list: {},
 			dog_list: {},
+			page_count: 1,
 		},
 		mutations: {
 			breed_list_update(state, payload) {
-					state.breed_list = {...payload}
+				state.breed_list = {...payload}
 			},
 			dog_list_update(state, payload) {
-					state.dog_list = [...payload]
+				state.dog_list = [...payload]
 			},
+			page_count_update(state, payload) {
+				state.page_count = parseInt(payload);
+			}
 		}
 	})
 };
